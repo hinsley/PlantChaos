fa5dbba2587125e57b63979907d7ad08f5bf4e6d
Carter Hinsley <chinsley1@student.gsu.edu>
2023-02-05 18:06:15 -0500

    Correction to chunk offset due to iteration range

--- toys/spike_count_sweep.jl
+++ toys/spike_count_sweep.jl
@@ -150,11 +150,11 @@ tspan = (0.0f0, 1.0f5)
 for chunk in 0:Int(1/chunk_proportion)^2-1
     println("Beginning chunk $(chunk+1) of $(Int(1/chunk_proportion)^2).")
     params = []
-    chunk_ΔCa_min = ΔCa_min + (ΔCa_max - ΔCa_min)*chunk_proportion*trunc(Int, chunk*chunk_proportion)
-    chunk_ΔCa_max = ΔCa_min + (ΔCa_max - ΔCa_min)*(chunk_proportion*(trunc(Int, chunk*chunk_proportion)+1)-1/ΔCa_resolution)
+    chunk_ΔCa_min = ΔCa_min + (ΔCa_max - ΔCa_min)*chunk_proportion*trunc(Int, (chunk+1)*chunk_proportion)
+    chunk_ΔCa_max = ΔCa_min + (ΔCa_max - ΔCa_min)*(chunk_proportion*(trunc(Int, (chunk+1)*chunk_proportion)+1)-1/ΔCa_resolution)
     @time for ΔCa in range(chunk_ΔCa_min, chunk_ΔCa_max, length=Int(ΔCa_resolution*chunk_proportion))
-        chunk_Δx_min = Δx_min + (Δx_max - Δx_min)*chunk_proportion*(chunk%(1/chunk_proportion))
-        chunk_Δx_max = Δx_min + (Δx_max - Δx_min)*(chunk_proportion*(chunk%(1/chunk_proportion)+1) - 1/Δx_resolution)
+        chunk_Δx_min = Δx_min + (Δx_max - Δx_min)*chunk_proportion*((chunk+1)%(1/chunk_proportion)-1)
+        chunk_Δx_max = Δx_min + (Δx_max - Δx_min)*(chunk_proportion*((chunk+1)%(1/chunk_proportion)) - 1/Δx_resolution)
         for Δx in range(chunk_Δx_min, chunk_Δx_max, length=Int(Δx_resolution*chunk_proportion))
             p = @SVector Float32[
                 Plant.default_params[1],  # Cₘ
