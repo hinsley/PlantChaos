ae96d0fb58f105d162fc109e920d71e8bfcdcbd7
Carter Hinsley <chinsley1@student.gsu.edu>
2023-04-01 15:48:09 -0400

    Add remaining parameter sweeps

---
+++ toys/output/Horizontal AH strip/parameters.jl
@@ -0,0 +1,9 @@
+ΔCa_min = -50.0
+ΔCa_max = 370.0
+ΔCa_resolution = 1600
+Δx_min = -5.0
+Δx_max = 1.0
+Δx_resolution = Int(ΔCa_resolution/2)
+chunk_proportion = 1/8
+
+tspan = (0.0f0, 1.0f5)
---
+++ toys/output/Vertical BT strip/parameters.jl
@@ -0,0 +1,9 @@
+ΔCa_min = -50.0
+ΔCa_max = 10.0
+ΔCa_resolution = 1600
+Δx_min = -15.0
+Δx_max = 5.0
+Δx_resolution = Int(ΔCa_resolution/2)
+chunk_proportion = 1/8
+
+tspan = (0.0f0, 1.0f5)
