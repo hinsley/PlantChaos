e4301b00deb56544a888bae53e7b42160e9ee53f
dbloom2 <dbloom2@student.gsu.edu>
2023-04-14 13:25:31 -0400

    Keep track of current bifurcation parameter selection  #3

--- explorer/bifurcation.jl
+++ explorer/bifurcation.jl
@@ -9,7 +9,11 @@ scatter!(bifax, hopf[1,2], hopf[2,2], color=:blue, marker=:star4, label="BT", ma
 scatter!(bifax, hopf[1,1063], hopf[2,1063], color=:red, marker=:star6, label="GH", markersize=16)
 scatter!(bifax, hopf[1,8011], hopf[2,8011], color=:green, marker=:star8, label="GH", markersize=16)
 
-axislegend(bifax ,position=:lb)
+bifaxpoint = @lift Point2f( $p[17],$p[16])
+scatter!(bifax, bifaxpoint)
+
+axislegend(bifax, position=:lb)
+
 limits!(bifax, -150, 400, -20, 50)
 
 bifpoint = select_point(bifax.scene, marker = :circle)
@@ -18,4 +22,5 @@ on(bifpoint) do pars
     delCa, delx = pars
     bifax.title = "Bifurcation Diagram (ΔCa: $delCa, Δx: $delx)"
     p[] = (p[][1:end-2]..., delx, delCa)
+    bifaxpoint[] = [delCa, delx]
 end
