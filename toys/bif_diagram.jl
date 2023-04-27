a48d5f598afdbf7609860b1b5d6ea959ff2162d4
Carter David Hinsley <carterhinsley@outlook.com>
2023-04-27 13:39:30 -0400

    Add display for codim 2 points

--- toys/bif_diagram.jl
+++ toys/bif_diagram.jl
@@ -15,3 +15,8 @@ function plot_bif_diagram!(plt)
     scatter!(plt, [hopf[1,1063]], [hopf[2,1063]], color=:red, marker=:star6, label="GH", markersize=16)
     scatter!(plt, [hopf[1,8011]], [hopf[2,8011]], color=:green, marker=:star8, label="GH", markersize=16)
 end
+
+function get_codim_2_bifn_params()
+    hopf = readdlm("./explorer/hopf.csv", ',', Float64)
+    return (hopf[1,2], hopf[2,2]), (hopf[1,1063], hopf[2,1063]), (hopf[1,8011], hopf[2, 8011])
+end
