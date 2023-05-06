e6f4fbc353138c45a165a85d227b162b17e58dc7
Carter Hinsley <chinsley1@student.gsu.edu>
2023-05-05 23:03:19 -0400

    Add Full poster sample scan

--- toys/spike_count_sweep.jl
+++ toys/spike_count_sweep.jl
@@ -425,17 +425,17 @@ function makeParams(ΔCa, Δx)
     ]
 end
 
-ΔCa_min = -150.0
-ΔCa_max = 370.0
-ΔCa_resolution = 1600
-Δx_min = -20.0
-Δx_max = 30.0
+ΔCa_min = -130.0
+ΔCa_max = 100.0
+ΔCa_resolution = 2000
+Δx_min = -12.0
+Δx_max = 15.0
 Δx_resolution = Int(ΔCa_resolution/2)
 chunk_proportion = 1/8
 
 tspan = (0.0f0, 1.0f5)
 
-scan_directory = "toys/output/Full parameter plane"
+scan_directory = "toys/output/Full poster sample"
 
 for chunk in 0:Int(1/chunk_proportion)^2-1
     println("Beginning chunk $(chunk+1) of $(Int(1/chunk_proportion)^2).")
