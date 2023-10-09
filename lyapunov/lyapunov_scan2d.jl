4bb0ec46ba4da810fe3316d147a5d996e1aa657a
Jack Scully <jamesjscully@gmail.com>
2023-10-09 15:40:13 -0400

    after rebase working on lyap

--- lyapunov/lyapunov_scan2d.jl
+++ lyapunov/lyapunov_scan2d.jl
@@ -4,7 +4,7 @@ using Pkg
 Pkg.activate("./lyapunov")
 using GLMakie, StaticArrays, ProgressMeter, DynamicalSystems
 using LinearAlgebra: norm
-using OrdinaryDiffEq: RK4
+using OrdinaryDiffEq
 include("../model/Plant.jl")
 include("./my_lyapunov.jl")
 
@@ -13,41 +13,33 @@ start_p = Float64[Plant.default_params...]
 start_p[17] = -70.0 # Cashift
 start_p[16] = -4. # xshift
 end_p = Float64[Plant.default_params...]
-end_p[17] = 100.0 # Cashift
+end_p[17] = 150.0 # Cashift
 end_p[16] = 1. # xshift
 
-resolution = 30 # How many points to sample.
+resolution = 1000 # How many points to sample.
 Ca_shifts = LinRange(start_p[17], end_p[17], resolution)
 x_shifts = LinRange(start_p[16], end_p[16], resolution)
 ps = [[Plant.default_params[1:15]; [x_shifts[i], Ca_shifts[j]]] for i in 1:resolution, j in 1:resolution]
 
 # set up systems
-u0 = @SVector Float64[
-    0.2, # x
-    Plant.default_state[2],
-    Plant.default_state[3],
-    Plant.default_state[4],
-    1.0, # Ca
-    Plant.default_state[6],
-    #Plant.default_state[7]
-]
-sys = CoupledODEs(Plant.melibeNew, u0, ps[1]; diffeq = (alg = RK4(), adaptive = false, dt = 1.0))
-d0 = 1e-6
-_d1 = [randn(), 0., randn(4)...]
+u0 = rand(5)
+sys = CoupledODEs(Plant.melibeNew, u0, ps[1]; diffeq = (alg = BS3(), dtmax = 6.0, abstol = 1e-7, reltol = 1e-7))
+d0 = 1e-8
+_d1 = randn(5)
 _d2 = _d1/norm(_d1)*d0
 psys = ParallelDynamicalSystem(sys, [u0, u0 + _d2])
 systems = [deepcopy(psys) for _ in 1:Threads.nthreads() - 1]
 pushfirst!(systems, psys)
 
-@time my_lyapunov(systems[1], 150000, 50000, 1.0, d0 = d0, num_avg = 100, max_period = 20000)
-@time lyapunov(systems[1], 150000; Ttr = 50000, d0 = d0)
 traj = trajectory(sys, 150000)
 begin
     fig = Figure(resolution = (2000,2000))
     ax = Axis(fig[1,1], xlabel = "t", ylabel = "x")
-    lines!(ax, collect(traj[2]), traj[1][:,6])
+    lines!(ax, collect(traj[2]), traj[1][:,5])
     fig
 end
+
+lyapunov(sys, 150000; Ttr = 50000, d0 = d0)
 
 # perform scan
 lyaparray = zeros(Float64, resolution, resolution)
@@ -57,11 +49,13 @@ begin
         i = div(I - 1, resolution) + 1
         j = mod(I - 1, resolution) + 1
         system = systems[Threads.threadid()]
+        u0 = rand(6)
+        _d1 = [randn(), 0., randn(4)...]
+        _d2 = _d1/norm(_d1)*d0
         set_parameter!(system, 16, x_shifts[i])
         set_parameter!(system, 17, Ca_shifts[j])
-        break
-        lyaparray[i,j] = my_lyapunov(system, 150000, 50000, 1.0, d0 = d0, num_avg = 100, max_period = 10000)
-        set_state!(system, u0)
+        reinit!(system, u0, [SVector{6}(u0), SVector{6}(u0+_d2)])
+        lyaparray[i,j] = lyapunov(system, 500000; Ttr = 500000, d0 = d0)
         next!(p)
     end
     finish!(p)
@@ -71,8 +65,10 @@ end
 let fig = Figure(resolution = (2000,2000))
     ax = Axis(fig[1,1], xlabel = L"\Delta Ca", ylabel = L"\Delta x")
     hm = GLMakie.heatmap!(ax, Ca_shifts, x_shifts, lyaparray'; colorrange = (.0000,.00025), 
-    colormap = [:midnightblue, :lightskyblue1, :white, :yellow, :orange, :red], lowclip= :black)
+    colormap = [:midnightblue, :lightskyblue1, :white, :yellow, :orange, :red])
     Colorbar(fig[1,2], hm)
     fig
 end
+
+save(homedir() * "/Dropbox/lyapunov.png",fig)
 
--- lyapunov/test.jl
+++ lyapunov/test.jl
@@ -1,19 +1,21 @@
 function test_lyap_plot(sys, T, Ttr)
     d0 = 1e-6
-    _d1 = [randn(), 0., randn(4)..., 0.]
+    _d1 = [randn(), 0., randn(4)...]
     _d2 = _d1/norm(_d1)*d0
     psys = ParallelDynamicalSystem(sys, [u0, u0 + _d2])
     λs = Float64[]
+    traj = Float64[]
     for i in 1:T
         d = λdist(psys)/d0
         λrescale!(psys, d)
-        step!(psys)
+        step!(psys, 1.0)
         if i>Ttr
             push!(λs, log(d))
+            push!(traj, current_state(psys)[6])
         end
     end
     # build average of normalized cumsums
-    num_avg = 100
+    num_avg = 10
     max_period = 10000
     dt = 1.0
     max_i = ceil(Int, max_period/dt)
@@ -24,18 +26,15 @@ function test_lyap_plot(sys, T, Ttr)
         λtot = λtot .+ cumsum(λs[i:end-max_i+i-1])./collect(1:(length(λs)-max_i))
     end
     λtot = λtot ./ length(idxs)
-    λs = λtot
 
     F = Figure()
-    traj = trajectory(sys, T, Δt = 1.0)
-    println(length(traj[1][:,6]))
     ax = Axis(F[1,1], xlabel = "Time", ylabel = "v")
-    #lines!(ax, traj[1][:,6]./maximum(traj[1][:,1]).*maximum(λs), color = :blue)
-    ais2 = vcat(zeros(Ttr), λs)
-    #lines!(ax, ais2, color = :red)
-    lines!(ax, λs, color = :green)
-    λs = vcat(zeros(length(λs)-max_i), cumsum(λs[end-max_i:end-1])./collect(1:max_i))
-    lines!(ax, λs, color = :blue)
+
+    lines!(ax, traj, color = :blue)
+    #lines!(ax, λs, color = :red)
+    lines!(ax, λtot, color = :green)
+    λ2 = vcat(zeros(length(λtot)-max_i), cumsum(λtot[end-max_i:end-1])./collect(1:max_i))
+    lines!(ax, λ2, color = :blue)
     F
 end
-test_lyap_plot(sys, 100000, 10000)
+test_lyap_plot(sys, 150000, 50000)
