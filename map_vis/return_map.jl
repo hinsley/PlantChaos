ab1df7302791335819ac0f23fd159babdf2bde3b
Jack Scully <jamesjscully@gmail.com>
2023-11-03 17:49:37 -0400

    saddle trajectories

--- map_vis/return_map.jl
+++ map_vis/return_map.jl
@@ -1,6 +1,6 @@
 
-map_resolution = 100
-max_minima = 10
+map_resolution = 200
+max_minima = 15
 
 # generate initial conditions
 
@@ -81,20 +81,6 @@ monteprob = @lift EnsembleProblem($map_prob, prob_func=prob_func, output_func= o
 mapsol = @lift solve($monteprob, RK4(),EnsembleThreads(), trajectories=map_resolution,
     callback=cb, merge_callbacks = true, verbose=false, abstol = 1e-8, reltol = 1e-8)
 
-# calculate the trajectories leaving from the unstable manifold of the saddle point if it exists
-function get_saddle(prob, p)
-    v_eqs = find_zeros(v -> Ca_difference(p, v), Plant.xinfinv(p, 0.99e0), Plant.xinfinv(p, 0.01e0))
-    if length(v_eqs) < 5
-        return fill(NaN, 6)
-    end
-    v_eq = v_eqs[3]
-    Ca_eq = Equilibria.Ca_null_Ca(p, v_eq)
-    x_eq = Plant.xinf(p, v_eq)
-    return eq = [x_eq, 0.0, Plant.ninf(v_eq), Plant.hinf(v_eq), Ca_eq, v_eq]
-end
-
-saddle = @lift get_saddle(_eq_prob, $p)
-
 # ensure that the map is recalculated whent the map limits are changed
 onany(mapslider.sliders[1].value, mapslider.sliders[2].value) do val, _
     mapsol[] = solve(monteprob[], RK4(),EnsembleThreads(), trajectories=map_resolution[],
@@ -103,6 +89,7 @@ onany(mapslider.sliders[1].value, mapslider.sliders[2].value) do val, _
     reset_limits!(trajax)
 end
 
+# plot the map
 preimage = @lift getindex.($mapics, 1)
 
 cass = @lift([$mapsol[i][1,:] for i in 1:map_resolution])
@@ -111,12 +98,10 @@ vss = @lift([$mapsol[i][3,:] for i in 1:map_resolution])
 
 xmap = @lift [$mapsol[i][2,end] for i in 1:map_resolution]
 
-function calculate_hom_box(xmap, preimage)
-    d = diff(xmap.- preimage)
-    i = findfirst(i -> d[i]*d[i+1] < 0, 1:(length(d)-1))
-    return preimage[i], preimage[i+1], xmap[i], xmap[i+1]
-end
+lines!(mapax, preimage, xmap, color = range(0.,1., length=map_resolution), colormap = :thermal)
+lines!(mapax, preimage, preimage, color = :white, linestyle = :dash, linewidth = 2,)
 
+# plot the trajectories
 
 function glue_trajs(mapsol)
     cass = Float64[]
@@ -138,16 +123,81 @@ cass = @lift $_ans[1]
 xss = @lift $_ans[2]
 vss = @lift $_ans[3]
 
-#plot
-lines!(mapax, preimage, xmap, color = range(0.,1., length=map_resolution), colormap = :thermal)
-lines!(mapax, preimage, preimage, color = :white, linestyle = :dash, linewidth = 2,)
-
-colorrng = @lift range(0,1, length = length($cass))
-
+colorrng = range(0,1, length = length(cass[]))
 lines!(trajax, cass, xss, vss, color = colorrng, colormap = :thermal,
- linewidth = 1.0, fxaa = false, alpha = 0.5)
+    linewidth = .2)
 
-function refine_map!(mapsol, mapics, p, eq, xs, Ca, res)
+# calculate the trajectories leaving from the unstable manifold of the saddle point if it exists
+using LinearAlgebra, ForwardDiff
+function get_saddle_traj(prob,p)
+    v_eqs = find_zeros(v -> Equilibria.Ca_difference(p, v), Plant.xinfinv(p, 0.99e0), Plant.xinfinv(p, 0.01e0))
+    if length(v_eqs) < 5
+        return fill(NaN, 6)
+    end
+    v_eq = v_eqs[3]
+    Ca_eq = Equilibria.Ca_null_Ca(p, v_eq)
+    x_eq = Plant.xinf(p, v_eq)
+    saddle = [x_eq, 0.0, Plant.ninf(v_eq), Plant.hinf(v_eq), Ca_eq, v_eq]
+    jac = ForwardDiff.jacobian(u -> melibeNew(u,p,0), saddle)
+    vals,vecs = eigen(jac)
+    _,i = findmax(real.(vals))
+    eps = .001
+    upper_u0 = SVector{6}(saddle .+ eps .* real.(vecs)[:,i])
+    lower_u0 = SVector{6}(saddle .- eps .* real.(vecs)[:,i])
+    upper_prob = remake(prob, u0 = upper_u0, p = (p = p, eq = prob.p.eq))
+    upper_sol = solve(upper_prob, RK4(), abstol = 1e-8, reltol = 1e-8, callback = cb)
+    lower_prob = remake(prob, u0 = lower_u0, p = (p = p, eq = prob.p.eq))
+    lower_sol = solve(lower_prob, RK4(), abstol = 1e-8, reltol = 1e-8, callback = cb)
+    return (upper_sol[[5,1,6],:], lower_sol[[5,1,6],:])
+end
+
+sad_traj = @lift get_saddle_traj($map_prob, $p)
+
+
+sad_upper = @lift $sad_traj[1]
+sad_lower = @lift $sad_traj[2]
+
+lines!(trajax, sad_upper, color = :white, linewidth = 1.0)
+lines!(trajax, sad_lower, color = :white, linewidth = 1.0)
+
+ln1 = @lift [
+    ($preimage[end], $sad_upper[2,end]),
+    ($preimage[1], $sad_upper[2,end]),
+    (NaN,NaN),
+    ($preimage[end], $sad_lower[2,end]),
+    ($preimage[1], $sad_lower[2,end]),
+    (NaN,NaN),
+    ($preimage[end], $xmap[1]),
+    ($preimage[1], $xmap[1]),
+]
+#line extending from origin
+lines!(mapax, ln1, color = :white, linewidth = 1.0, linestyle = :dash)
+
+function calculate_hom_box(xmap, preimage)
+    d = xmap.- preimage
+    ixs = findall(i -> d[i]*d[i+1] < 0, 2:(length(d)-1))
+    if length(ixs) < 2 
+        return [[(NaN,NaN), (NaN,NaN)], [(NaN,NaN), (NaN,NaN)]]
+    end
+    (minx, mini) = findmin(xmap[1:ixs[end]])
+    lns = []
+    push!(lns, [(minx,minx), (preimage[mini], minx)])
+    push!(lns, [(preimage[ixs[1]], preimage[ixs[1]]), (minx, preimage[ixs[1]])])
+    return lns
+end
+
+lns = @lift calculate_hom_box($xmap, $preimage)
+ln2 = @lift $lns[1]
+ln3 = @lift $lns[2]
+
+#line calculated as minmum of map
+lines!(mapax, ln2, color = :pink, linestyle = :dash, linewidth = 1)
+lines!(mapax, ln3, color = :red, linestyle = :dash, linewidth = 1)
+
+
+
+
+function refine_map!(prob, mapsol, mapics, p, eq, xs, Ca; iters = 10)
     ics = Vector{SVector{6,Float64}}(undef, res)
     Threads.@threads for i=1:length(xs)
         fastu = solve(
